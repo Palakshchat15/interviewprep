dd7741cdf1f33bd2fea8be19ccb6ca2fa2875fff
Palaksh <palakshchaturvedi2022@vitbhopal.ac.in>
2025-06-11 20:32:40 +0530

    second

--- frontend/src/pages/Home/Dashboard.jsx
+++ frontend/src/pages/Home/Dashboard.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { LuPlus } from "react-icons/lu";
 import { CARD_BG } from "../../utils/data";
 import toast from "react-hot-toast";
-import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
@@ -11,6 +10,8 @@ import moment from "moment";
 import Modal from "../../components/Modal";
 import CreateSessionForm from "./CreateSessionForm";
 import DeleteAlertContent from "../../components/DeleteAlertContent";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+
 
 const Dashboard = () => {
   const navigate = useNavigate();
